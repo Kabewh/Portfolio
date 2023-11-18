4bb9e71760ff75658e6f45bcd32b53bd93c94e73
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-11-18 23:35:53 +0200

    changed pics

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -3,10 +3,10 @@ const Project = ({ picture, description }) => {
   return (
     <div className="flex flex-col md:grid grid-cols-2">
       <div className="flex md:justify-end">
-        <img className="lg:w-1/2" src={picture}></img>
+        <img className="-mt-7 lg:w-1/2" src={picture}></img>
       </div>
-      <div className="lg:w-1/2 h-auto lg:h-48 md:flex text-sm md:text-lg text-center md:text-start items-start md:mt-28 justify-center md:justify-start">
-        <span className="w-full break-words">{description}</span>
+      <div className="lg:w-1/2 h-auto md:flex text-sm md:text-lg text-center md:text-start items-start justify-center md:justify-start">
+        <span className="break-words">{description}</span>
       </div>
     </div>
   );
